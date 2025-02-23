457a77e102d9735cc77884b9989c4e15d955815e
DimitriTedom <dimitritedom@gmail.com>
2025-02-23 07:01:03 +0100

    feat(Gradient2.tsx,Gradient1.tsx...) Implemented 1 sub section with success

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -1,3 +1,5 @@
+import Gradient1 from './Gradient1'
+import Gradient2 from './Gradient2'
 import RecentWork from './RecentWork'
 import './Section.css'
 const Section = () => {
@@ -5,7 +7,7 @@ const Section = () => {
     <div className="Container">
       <div className='flex flex-col gap-6 cardBorder bg-CardBgColor  py-[20px] px-4 rounded-lg lg:px-11 lg:py-14'>
         <div className='flex items-center gap-2 '>
-        <div className="w-2 h-2 bg-TertiarTextColor rounded-full"></div>
+        <div className="w-4 h-4 bg-TertiarTextColor rounded-full buble"></div>
         <h1 className='text-TertiarTextColor font-bold md:text-2xl'>Recent Work</h1>
         </div>
 
@@ -14,13 +16,13 @@ const Section = () => {
         </div>
       </div>
 
-      {/* <div className='flex flex-col gap-2'>
-        <Gradient2 sens="tr" content="Product design"/>
-        <div>
-          <Gradient1 sens="tr" content="User Experience"/>
-          <Gradient1 sens="bl" content="NoCode develop"/>
+      <div className='flex flex-col gap-2'>
+        <Gradient2 content="Product design"/>
+        <div className='grid grid-cols-2 gap-3 lg:gap-5'>
+          <Gradient1  content="User Experience" />
+          <Gradient1  content="NoCode develop"/>
         </div>
-      </div> */}
+      </div>
     </div>
   )
 }
